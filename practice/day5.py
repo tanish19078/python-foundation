e7e4671e16ed80cf6b013ba1b3bd03ada79ccc6d
Tanish Singla <mastertanish100@gmail.com>
2025-09-15 12:22:41 +0530

    Add files via upload

---
+++ practice/day5.py
@@ -0,0 +1,30 @@
+# for i in range (3):
+    # for j in range(3):
+        # print(i, j)
+
+# making right angle triangle of *
+# for i in range(4):
+    # for j in range (i+1):
+        # print("*", end="")
+    # print()
+
+# print 10 times a number counting
+
+# x=int(input("Enter a number: "))
+
+# n=min(x,10)
+
+# for i in range(n):
+#  for j in range(i+1):
+    # print(j+1, end=" ")
+#  print()
+
+# inverted pattern - 4444>3333>22>1
+
+for i in range(4,0,-1):
+
+    for j in range(i):
+
+        print(i, end="")
+    print()
+
---
+++ practice/day6.py
@@ -0,0 +1,33 @@
+# functions in python are defined using the def keyword
+
+# factorial of numbers upto 996 as max recursion depth exceeds up >996
+
+# n=int(input("Enter a number: "))
+# def factorial(n):
+    # if n == 0 or n == 1:
+        # return 1
+    # else:
+        # return factorial(n-1) * n
+# print(factorial(n))
+
+# lcm of two numbers
+
+# x=int(input("Enter first number: "))
+# y=int(input("Enter second number: "))
+
+# def lcm(x, y):
+    # if x > y:
+        # greater = x
+    # else:
+        # greater = y
+
+    # while True:
+        # if greater % x == 0 and greater % y == 0:
+            # lcm = greater
+            # break
+        # greater += 1
+
+    # return lcm
+
+# print(f"The LCM of {x} and {y} is {lcm(x, y)}")
+
